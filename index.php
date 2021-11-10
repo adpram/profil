78717f057e5dce6d5b870f6902bacceb2017b1fb
Adi Pramono <adpram46@gmail.com>
2021-11-10 23:00:44 +0700

    update new design

--- index.php
+++ index.php
@@ -6,100 +6,100 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halo</title>
-    <link rel="stylesheet" href="assets/bootstrap-5.1.0/dist/css/bootstrap.min.css" type="text/css" />
-    <link rel="stylesheet" href="assets/fontawesome-free-5.15.4-web/css/all.css">
-    <link rel="stylesheet" href="prum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+    <link href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
 
 <body>
-    <div class="row" id="main">
-        <div class="col-md-2"></div>
-        <div class="col-md-4" id="left-side">
-            <table width="100%">
-                <tr>
-                    <th>
-                        <span class="text-color name">Adi Pramono</span>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="center">
-                        <img src="assets/img/profil.jpeg" alt="profil" class="profil-pict">
-                    </th>
-                </tr>
-                <tr>
-                    <th class="center">
-                        <p class="text-color desc">Seorang mahasiswa semester akhir di salah satu universitas swasta di
-                            Surabaya. Tertarik dengan pemrograman terutama website dan suka jalan-jalan.</p>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="center">
-                        <p class="text-color desc">mari berteman</p>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="center">
-                        <a href="https://www.instagram.com/adpram_/" style="padding: 8px;" target="_blanks"><i
-                                class="fab fa-instagram text-white fa-lg"></i></a>
-                        <a href="https://www.linkedin.com/in/adi-pramono-9b0b3b1b2/" style="padding: 8px;" target="_blanks"><i
-                                class="fab fa-linkedin text-white fa-lg"></i></a>
-                        <a href="https://github.com/adpram" style="padding: 8px;" target="_blanks"><i
-                                class="fab fa-github text-white fa-lg"></i></a>
-                    </th>
-                </tr>
-            </table>
-        </div>
-        <div class="col-md-4" id="right-side">
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                        aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
+    <div class="d-flex align-items-center justify-content-center" id="parent">
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-5" id="div-profil">
+                    <img src="assets/img/profil.jpeg" alt="profil" class="profil-pict">
+                    <p class="nama">Adi<br>Pramono</p>
+                    <hr>
+                    <p>J U N I O R &nbsp; P R O G R A M M E R</p>
+                    <br>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="/assets/img/sistemgudang.png" class="d-block image-slide" alt="sistem pergudangan">
-                        <div class="carousel-caption d-none d-md-block desc-slide">
-                            <h5><b>Sistem Pergudangan</b></h5>
-                            <p><b>Pencatatan mutasi barang PT. Emdeki Utama tbk.</b></p>
-                        </div>  
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/assets/img/sukorejo.png" class="d-block image-slide" alt="sukorejo">
-                        <div class="carousel-caption d-none d-md-block desc-slide">
-                            <h5><b>Website Desa</b></h5>
-                            <p><b>Berisi profil dan produk umkm desa Sukorejo.</b></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/assets/img/asa.png" class="d-block image-slide" alt="asa">
-                        <div class="carousel-caption d-none d-md-block desc-slide">
-                            <h5><b>Website Perusahaan</b></h5>
-                            <p><b>Profil dan produk jasa PT. Aria Sumit Andalan.</b></p>
-                        </div>
+                <div class="col-md-7 my-auto" id="div-desc">
+                    <p class="halo">Halo</p>
+                    <p class="me">Saya seorang mahasiswa semester akhir di salah satu universitas swasta di Surabaya. Tertarik dengan pemrograman terutama website. Pernah bekerja di perusahaan <i>startup</i> yang bergerak di bidang klinik estetika <i>POS</i> dan <i>management system</i> sebagai <i>junior programmer</i> selama 1 tahun.</p>
+                    <div class="mt-3">
+                        <ul class="social">
+                            <li>
+                                <a href="https://www.instagram.com/adpram_/" target="_blanks"><i
+                                            class="fa fa-instagram fa-lg text-black"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/adi-pramono-9b0b3b1b2/" target="_blanks"><i
+                                            class="fa fa-linkedin fa-lg text-black"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/adpram" target="_blanks"><i
+                                            class="fa fa-github fa-lg text-black"></i></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
         </div>
-        <div class="col-md-2"></div>
     </div>
-
-
-
-    <script src="assets/bootstrap-5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="col-md-8">
+            <div class="row">
+                <h3><span id="list-buatan">hai</span> <b>Projects</b></h3>
+                <div class="col-md-5 mt-4" id="div-project">
+                    <p class="judul-portofolio">Sistem Pergudangan</p>
+                    <span class="badge bg-primary sub-judul-portofolio">Laravel 5.8</span>
+                    <span class="badge bg-primary sub-judul-portofolio">MySql</span>
+                    <p class="deskripsi-portofolio">
+                        [2021] Digunakan untuk pencatatan mutasi stok masuk dan keluar barang. Tersedia 2 akun, admin dan staf gudang                    
+                        <ul>
+                            <li>Admin : bisa melakukan pencatatan mutasi barang dan mengelola lokasi barang tersebut disimpan yakni area, rak dan lokasin bin.</li>
+                            <li>Staf gudang : hanya bisa melakukan pencatatan mutasi barang dan melihat lokasi barang berada.</li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="col-md-7 mt-4" id="div-image-portofolio">
+                    <img src="assets/img/1.png" alt="" id="portofilio-image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 mt-5" id="div-project">
+                    <p class="judul-portofolio">Website Desa</p>
+                    <span class="badge bg-primary sub-judul-portofolio">CodeIgniter 3</span>
+                    <span class="badge bg-primary sub-judul-portofolio">MySql</span>
+                    <p class="deskripsi-portofolio">
+                        [2020] Berisi profil dan produk umkm desa. Dashboard berisi profil, perangkat desa dan produk-produk umkm warga, dimana orang bisa langsung menghubungi pemilik produk melalui whatsapp yang tertera. Terdapat 2 role, Perangkat desa dan Admin toko online
+                        <ul>
+                            <li>Perangkat desa : Mengkonfigurasi profil website, data perangkat desa dan Melakukan pencatatan data sipil penduduk desa.</li>
+                            <li>Admin toko online : Mengelola data produk umkm (kategori & produk).</li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="col-md-7 mt-5" id="div-image-portofolio">
+                    <img src="assets/img/2.png" alt="" id="portofilio-image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 mt-5" id="div-project">
+                    <p class="judul-portofolio">Website Perusahaan</p>
+                    <span class="badge bg-primary sub-judul-portofolio">CodeIgniter 3</span>
+                    <span class="badge bg-primary sub-judul-portofolio">MySql</span>
+                    <p class="deskripsi-portofolio">
+                        [2020] Berisi profil dan produk yang dijual perusahaan. User admin digunakan untuk mengelola produk perusahaan yang ditampilkan di website
+                    </p>
+                </div>
+                <div class="col-md-7 mt-5" id="div-image-portofolio">
+                    <img src="assets/img/3.png" alt="" id="portofilio-image">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
